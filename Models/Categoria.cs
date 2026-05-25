ffdcabd17dd404b88448cf854d5fcac8c418c9b1
Fabio Lima <silva.l3@hotmail.com>
2026-05-24 21:21:39 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Categoria.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Models
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+
+        [Key]
+        public int CategoriaID { get; set; }
+        [Required]
+        [StringLength(80)]
+        public string? Nome { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string? ImageUrl { get; set; }
+        public ICollection<Produto>? Produtos { get; set; }
+    }
+}
