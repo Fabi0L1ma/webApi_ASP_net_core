c903df0046800b49b25ea54746430770c808e4e3
Fabio Lima <silva.l3@hotmail.com>
2026-05-26 09:07:58 -0300

    wip: migrations

---
+++ Migrations/20260526042153_PopulaCategoria.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into \"Categorias\"(\"Nome\", \"ImageUrl\") values('Sobremesa', 'sobremesa.jpg')\r\n");
+            //migrationBuilder.Sql("insert into Categorias(Nome, ImageUrl) values('Lanche', 'lanche.jpg')");
+            //migrationBuilder.Sql("insert into Categorias(Nome, ImageUrl) values('Sobremesa', 'sobremesa.jpg')");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from Categorias");
+        }
+    }
+}
