601c85bdacacea52dc7d202b187af4138f268bdb
Fabio Lima <silva.l3@hotmail.com>
2026-05-30 15:06:20 -0300

    feat: criando metodos GETS e POST

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Context;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutoController(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+
+            if(produtos is null)
+            {
+                return NotFound("Produtos não encontrados.");
+            }
+
+            return produtos;
+        }
+
+        [HttpGet("{id:int}", Name="ObterProduto")]
+        public ActionResult<Produto> GetId(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if(produto is null)
+            {
+                return NotFound("Produto não encontrado.");
+            }
+
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if(produto is null)
+            {
+                return BadRequest();
+            }
+            _context.Produtos.Add(produto);
+            
+            _context.SaveChanges();
+
+            return new CreatedAtActionResult("ObterProduto", null, new { id = produto.ProdutoId }, produto);
+        }
+    }
+}
