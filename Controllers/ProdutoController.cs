71a1ecc6c88ace98d52c9a4d81400a92f0d9f6a1
Fabio Lima <silva.l3@hotmail.com>
2026-05-31 00:32:09 -0300

    feat: adicionando metodo de deletar e editar produto

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApi.Context;
 using WebApi.Models;
 
@@ -54,6 +55,37 @@ namespace WebApi.Controllers
             _context.SaveChanges();
 
             return new CreatedAtActionResult("ObterProduto", null, new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if(id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if(produto is null)
+            {
+                return NotFound("Produto não localizado.");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
         }
     }
 }
